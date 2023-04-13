27ebd7e94a969f8e85cbbdba1b415be91758d186
Melal1 <105118661+Melal1@users.noreply.github.com>
2023-04-13 07:40:08 +0300

    nvim: moved nvimold to home/git directory

--- nvim/.config/nvimold/init.lua
+++
@@ -1,7 +0,0 @@
-require('Melal.plugins')
-require('Melal.options')
-require('Melal.lualine-config')
-require('Melal.buffer-conf')
-require('Melal.treesitter-config')
-vim.cmd('colorscheme melange')
-
