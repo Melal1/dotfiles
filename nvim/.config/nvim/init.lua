eeb26494ee586a5e2b99cd3eceda53f0ad84a02b
Melal1 <105118661+Melal1@users.noreply.github.com>
2023-04-13 07:31:05 +0300

    nvim:created option and mappings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,2 @@
-require('Melal.plugins')
 require('Melal.options')
-require('Melal.lualine-config')
-require('Melal.buffer-conf')
-require('Melal.treesitter-config')
-vim.cmd('colorscheme melange')
-
+require('Melal.mappings')
